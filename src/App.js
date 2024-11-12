a655ba57e83155caa2a3859ce5692be9548e5111
Nicol Lee <nicol.lee.91@gmail.com>
2024-11-12 23:26:10 +0800

    Change BrowserRouter to HashRouter Modified url for images in Home

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { HashRouter as Router, Routes, Route } from "react-router-dom"
 import { Home } from "./page/Home"
 import { Contact } from "./page/Contact"
 import { Works } from "./page/Works"
@@ -6,6 +6,7 @@ import { NotFound } from "./page/NotFound"
 import { NavBar } from "./component/NavBar"
 import "./css/App.css";
 
+const routerBaseName = process.env.PUBLIC_URL;
 
 function App() {
     return (
